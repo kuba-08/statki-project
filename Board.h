39564a23131b590313b75b55a424a1991cd5c606
kuba-08 <kubakieca2008@gmail.com>
2024-11-27 16:19:30 +0100

    Board.h

--- Board.h
+++ Board.h
@@ -4,19 +4,19 @@ class Board {
 private:
 
     char grid[10][10];  // Plansza gry
-    int rows, cols;
+    
     
 public:
-    Board(int rows = 10, int cols = 10);
-    void placeShip(int row, int col, int size, bool horizontal);
-    bool receiveShot(int row, int col);
-    bool areAllShipsSunk() const;
-    void markSurroundingArea(int row, int col, int size, bool horizontal); 
-    void printBoard(bool revealShips) const;
+    Board();
+    void printBoard(bool reveal) const;
+    bool placeShip(int x, int y, int size, bool horizontal);
+    bool isAreaClear(int x, int y, int size, bool horizontal) const;
     bool canPlaceShip(int x, int y, int size, bool horizontal) const;
-    bool hasbeenShotAt(int row, int col) const; // metoda sprawdzająca czy dane pole było już trafione
-    // void markAreaArrounShip(int x, int y, int size, bool horizontal); // funcja do umieszczania pudła wokó zatopionego statku
-    bool isAreaClear(int x, int y, int size , bool horizontal) const;
+    bool areAllShipsSunk() const;
+    bool receiveAttack(int x, int y);  // Odbiera atak i zwraca true, jeśli trafiony
+    bool hasbeenShotAt(int x, int y) const; // metoda sprawdzająca czy dane pole było już trafione
+   
+   
     
 
 
