cf3a2c7901eefeb1fc43dd1a5f610746453683a8
kuba-08 <kubakieca2008@gmail.com>
2024-12-01 13:15:39 +0100

    Board.cc

--- Board.cc
+++ Board.cc
@@ -19,7 +19,7 @@ void Board::printBoard(bool reveal) const {
         cout << i << " ";
         for (int j = 0; j < 10; ++j) {
             if (grid[i][j] == 'S' && !reveal) {
-                cout << "-";  // Ukrycie statków, jeśli reveal == false
+                cout << "- ";  // Ukrycie statków, jeśli reveal == false
             } else {
                 cout << grid[i][j] << " ";
             }
@@ -64,21 +64,45 @@ bool Board::isAreaClear(int x, int y, int size, bool horizontal) const {
 
 bool Board::canPlaceShip(int x, int y, int size, bool horizontal) const {
     if (horizontal) {
-        if (x + size > 10) return false;
-    } else {
-        if (y + size > 10) return false;
+        if (x + size > 10){
+            cout << "Statek wychodzi poza planszę wysokosciowo" << endl;
+        return false;
+        }  
+    }else {
+        if (y + size > 10){
+            cout << "Statek wychodzi poza plansze szerokosciowo" << endl;
+            return false;
+        }
     }
 
     if (horizontal) {
         for (int i = 0; i < size; ++i) {
-            if (grid[x + i][y] != '-') return false;
+            if (grid[x + i][y] != '-'){
+                cout << "nie można umiescic statku - pole (" << x + i << ", " << y << ") jest zajete!" << endl;
+                return false;
+            }
         }
     } else {
         for (int i = 0; i < size; ++i) {
-            if (grid[x][y + i] != '-') return false;
+            if (grid[x][y + i] != '-'){
+                return false;
+            }
         }
     }
+ for (int i = -1; i <= size; ++i) {
+        for (int j = -1; j <= 1; ++j) {
+            int checkX = horizontal ? x + i : x + j;
+            int checkY = horizontal ? y + j : y + i;
 
+            if (checkX >= 0 && checkX < 10 && checkY >= 0 && checkY < 10) { // Upewniamy się, że nie wychodzimy poza planszę
+                if (i >= 0 && i < size && j == 0) continue; // Ignorujemy pole, gdzie statek rzeczywiście będzie
+                if (grid[checkX][checkY] != '-') {
+                    cout << "Nie mozna umiescic statku - pole styka się z innym statkiem" << endl;     
+                    return false;
+                }
+            }
+        }
+    }
     return true;
 }
 
@@ -96,6 +120,7 @@ bool Board::areAllShipsSunk() const {
 bool Board::receiveAttack(int x, int y) {
     if (grid[x][y] == 'S') {
         grid[x][y] = 'X';  // Trafienie
+        
         return true;
     } else if (grid[x][y] == '-') {
         grid[x][y] = 'O';  // Pudło
@@ -107,5 +132,4 @@ bool Board::hasbeenShotAt(int x, int y) const
 {
     return grid[x][y] == 'X' || grid[x][y] == 'O';
 }
-
 
